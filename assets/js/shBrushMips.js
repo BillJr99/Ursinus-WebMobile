5950db50b69747673fd3a4cb7582ec7d38c16794
William Mongan <billmongan@gmail.com>
2022-01-06 09:58:56 -0500

    Brush

--- assets/js/shBrushMips.js
+++ assets/js/shBrushMips.js
@@ -7,7 +7,7 @@ var registers="v0 v1 a0 a1 a2 a3 t0 t1 t2 t3 t4 t5 t6 t7 t8 t9 s0 s1 s2 s3 s4 s5
 
 var opcodes="add addi addiu addu clo clz la li lui move negu seb seh sub subu rotr rotrv sll sllv sra srav srl srlv and andi ext ins nop nor not or ori wsbh xor xori movn movz slt slti sltiu sltu div divu madd maddu msub msubu mul mult multu mfhi mflo mthi mtlo b bal beq beqz bgez bgezal bgtz blez bltz bltzal bne bnez j jal jalr jr lb lbu lh lhu lw lwl lwr sb sh sw swl swr ulw usw ll sc";
 
-var misc=".text .data .asciiz .int .float";
+var misc="text data asciiz int float";
 
 this.regexList=[
 {regex:/^;.*$/gm,css:"comments"},
